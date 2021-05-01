740cccbe9806c624b4b1ea88c43e5031f81b94b0
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-05-01 10:34:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -97,3 +97,39 @@ right();
 console.log("right");
 }
 }
+function up(){
+    if(player_y>=0){
+        player_y=player_y-block_image_height;
+        console.log("block_image_height="+ block_image_height);
+        console.log("When Up arrow key is pressed, X="+ player_x+", Y="+ player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down(){
+    if(player_y<=500){
+        player_y=player_y+block_image_height;
+        console.log("block_image_height="+ block_image_height);
+        console.log("When Up arrow key is pressed, X="+ player_x+", Y="+ player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if(player_x>0){
+        player_x=player_x-block_image_width;
+        console.log("block_image_height="+ block_image_height);
+        console.log("When Up arrow key is pressed, X="+ player_x+", Y="+ player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if(player_y<=850){
+        player_x=player_x+block_image_width;
+        console.log("block_image_height="+ block_image_height);
+        console.log("When Up arrow key is pressed, X="+ player_x+", Y="+ player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
